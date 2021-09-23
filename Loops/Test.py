c11e96f969943466687c107b54eb3b16603ca877
Allan Zhou <allanlinzhou@gmail.com>
2021-09-23 11:18:11 -0400

    Added Loops

---
+++ Loops/Test.py
@@ -0,0 +1,19 @@
+for i in range(0, 12):
+    print(i, end=' ')
+print()
+
+for i in range(3, 14): 
+    print(i, end=' ')
+print()
+
+for i in range(0, 20, 2): 
+    print(i, end=' ')
+print()
+
+for i in range(10, 0, -1): 
+    print(i, end=' ')
+print()
+
+for i in range(3, 30, 3):
+    print(i, end=' ')
+print() 
