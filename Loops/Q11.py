7a4cfadefa28acbf4cacdeb3e2bb154d771e55b0
Allan Zhou <allanlinzhou@gmail.com>
2021-10-04 09:29:40 -0400

    Modified Loops

--- Loops/Q11.py
+++ Loops/Q11.py
@@ -1,9 +1,9 @@
 size = int(input("Triangle size: "))
 
-for i in range(1, size + 1): 
+# for i in range(1, size + 1): 
     # Print spaces to align * in the middle 
-    print(" "*(size - i), end = '')
-    print('* '*(i))
+    # print(" "*(size - i), end = '')
+    # print('* '*(i))
 
 def pyramid(size): 
     for i in range(1, size + 1): 
--- Loops/Q12.py
+++ Loops/Q12.py
@@ -1,11 +1,7 @@
 size = int(input("Triangle size: "))
 
-# for i in range(1, size + 1): 
-    # Print spaces to align * in the middle 
-    # print(" "*(size - i), end = '')
-    # print('* '*(i))
-
 def pyramid(size): 
+    print()
     for i in range(1, size + 1): 
         # Print spaces to align * in the middle 
         print(" "*(size - i), end = '')
