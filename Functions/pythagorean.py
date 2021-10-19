e372556de9894f1a33a72d8946e3687b200ef368
Allan Zhou <allanlinzhou@gmail.com>
2021-10-19 09:38:50 -0400

    Added New Multiple Check Function

--- Functions/pythagorean.py
+++ Functions/pythagorean.py
@@ -1,7 +1,7 @@
 def pythagorean(max_length): 
     """Prints all Pythagorean triples where the longest member is
     less than or equal to max_length. 
-    Time Complexity: O(n^3)"""
+    Time Complexity: O(n^4), where n is c."""
 
     for c in range(1, max_length + 1): 
         hyp_squared = c**2
@@ -10,24 +10,26 @@ def pythagorean(max_length):
         # Find all possible combinations of a and b. 
         for b in range(1, c): 
             for a in range(1, b + 1): 
+                # Triple satisfies a^2 + b^2 = c^2. 
+                if a**2 + b**2 == hyp_squared:
+                    multiple_check(a, b, c)
 
-                # Valid Pythagorean Triple. 
-                if a**2 + b**2 == hyp_squared: 
 
-                    # Do not print repeats. If a and b have a common 
-                    # divisor greater than 1, it will be a multiple of a 
-                    # previous triple. 
+def multiple_check(a, b, c): 
+    """Checks if a Pythagorean triple is a multiple of a lower triple.""" 
 
-                    for div in range(2, c): 
-                        if a % div == 0 and b % div == 0: 
-                            break
+    # If a and b have a common divisor greater than 1, it 
+    # will be a multiple of a previous triple. 
+    for div in range(2, c):
 
-                        # There are no common divisors. 
-                        elif div == c - 1: 
-                            print("({}, {}, {})".format(a, b, c))
+        # Common divisor with a and b, do not print triple.
+        if a % div == 0 and b % div == 0: 
+            break
 
-    print()
+        # There are no common divisors after all tests. 
+        elif div == c - 1: 
+            print("({}, {}, {})".format(a, b, c))
 
-                
+
 n = int(input("Enter a positive integer: "))
 pythagorean(n)
